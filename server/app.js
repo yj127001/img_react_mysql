1261f4af32345b33b626492e8d945cb2b1f73979
Your Name <you@example.com>
2023-07-27 18:36:41 +0800

    npm run build and Add client's public file to server

--- server/app.js
+++ server/app.js
@@ -14,52 +14,53 @@ const imgconfig = multer.diskStorage({
         return cb(null,`${Date.now()}-${file.originalname}`);
     }
   })
-
-  //img filter
-//   const isImage = (req,file,cb)=>{
-//     if(file.mimetype.startWith("image")){
-//         cb(null,true)
-//     }else{
-//         cb(null,Error('only image is allowed'))
-//     }
-//   }
-
-// const upload = multer({ storage: imgconfig ,fileFilter:isImage});
 const upload = multer({ storage: imgconfig });
 
+app.use(express.static('public'))
 
 app.post("/upload", upload.single('newFile'),(req, res) => {
     // use modules such as express-fileupload, Multer, Busboy
-    const imgname = req.file.originalname
-    if(!req.file){
-        res.status(422).json({result:422,msg:"please upload image"})
-    }
-    try{
-        conn.query("INSERT INTO usersdata SET ?",{userimg:imgname},(err,res)=>{
-            if(err){
-                console.log("ddddd"+imgname)
-            }else{
-                console.log("data added")
-            }
-        })
-    }catch(error){
-        res.status(422).json({result:422,error})
-    }
-    setTimeout(() => {
-        console.log('file uploaded')
-        return res.status(200).json({ result: true, msg: 'file uploaded' });
-    }, 3000);
+    // const imgname = req.file.originalname
+    // if(!req.file){
+    //     res.status(422).json({result:422,msg:"please upload image"})
+    // }
+    // try{
+    //     conn.query("INSERT INTO Images SET ?",{userimg:imgname},(err,res)=>{
+    //         if(err){
+    //             console.log("ddddd"+imgname)
+    //         }else{
+    //             console.log("data added")
+    //         }
+    //     })
+    // }catch(error){
+    //     res.status(422).json({result:422,error})
+    // }
+    // setTimeout(() => {
+    //     console.log('file uploaded')
+    //     return res.status(200).json({ result: true, msg: 'file uploaded' });
+    // }, 3000);
 });
 
-app.get("/getdata",(req,res)=>{
+app.get("/getdata",async ()=>{
     try{
-        conn.query("SELECT * FROM usersdata",(err,res)=>{
+        await conn.query(`SELECT * FROM Images`,(err,res)=>{
             if(err){
                 console.log("error")
             }else{
-                console.log(res)
+                console.log('this i'+res)
+                var columns = "";
+                for (var column in res.recordset.columns) {
+                    columns += column + ", ";
+                }
+                console.log("%s\t", columns.substring(0, columns.length - 2));
+        
+                // ouput row contents from default record set
+                res.recordset.forEach(row => {
+                    console.log("%s\t%s", row.CategoryName, row.ProductName);
+                });
             }
         })
+        console.log('nothing')
     }catch(error){
         res.status(422).json({result:422,error})
     }
--- server/db/conn.js
+++ server/db/conn.js
@@ -1,15 +1,55 @@
-const mysql = require("mysql");
+//https://learn.microsoft.com/en-us/azure/azure-sql/database/connect-query-nodejs?view=azuresql-mi&tabs=macos
 
-const conn = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"201@1!@Jj",
-    database:"userimgupload"
-});
+const sql = require('mssql');
 
-conn.connect((error)=>{
-    if(error) throw error;
-    console.log("connected1!")
-})
+const config = {
+    user: 'jing', // better stored in an app setting such as process.env.DB_USER
+    password: '201@1!@Jj', // better stored in an app setting such as process.env.DB_PASSWORD
+    server: 'jingdatabase.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: 'jingdatabase', // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
 
-module.exports = conn
+console.log("Starting...");
+connectAndQuery();
+
+async function connectAndQuery() {
+    try {
+        var poolConnection = await sql.connect(config);
+
+        console.log("Reading rows from the Table...");
+
+        var resultSet = await poolConnection.request().query(`SELECT * FROM Images`, function(err, recordset) {
+            if (err) console.log(err);
+            console.log(recordset);
+          });
+
+        console.log(`${resultSet.recordset.length} rows returned.`);
+
+        // output column headers
+        var columns = "";
+        for (var column in resultSet.recordset.columns) {
+            columns += column + ", ";
+        }
+        console.log("%s\t", columns.substring(0, columns.length - 2));
+
+        // ouput row contents from default record set
+        resultSet.recordset.forEach(row => {
+            console.log("%s\t%s", row.CategoryName, row.ProductName);
+        });
+
+        // close connection only when we're certain application is finished
+        poolConnection.close();
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+//***************************/
+module.exports = sql
