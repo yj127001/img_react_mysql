1ad4d4a44becd108924b047f4ba7b506bbd8d297
Your Name <you@example.com>
2023-07-28 08:59:40 +0800

    successfully connect to azure database according to officilal document

--- server/app.js
+++ server/app.js
@@ -1,77 +1,40 @@
+import express from 'express';
+import { config } from './db/config.js';
+import Database from './db/database.js';
 
-const express = require('express');
+// Import App routes
+import person from './db/person.js';
+import openapi from './db/opanapi.js';
+
+const port = process.env.PORT || 8080;
+
 const app = express();
-const conn = require('./db/conn')
-const multer  = require('multer')
-const cors = require('cors');
-app.use(cors());
 
-const imgconfig = multer.diskStorage({
-    destination: (req, file, cb) =>{
-      return cb(null, './uploads')
-    },
-    filename:(req, file, cb) =>{
-        return cb(null,`${Date.now()}-${file.originalname}`);
-    }
-  })
-const upload = multer({ storage: imgconfig });
+// Development only - don't do in production
+// Run this to create the table in the database
+if (process.env.NODE_ENV === 'development') {
+  const database = new Database(config);
+  database
+    .executeQuery(
+      `CREATE TABLE Person (id int NOT NULL IDENTITY, firstName varchar(255), lastName varchar(255));`
+    )
+    .then(() => {
+      console.log('Table created');
+    })
+    .catch((err) => {
+      // Table may already exist
+      console.error(`Error creating table: ${err}`);
+    });
+}
 
-app.use(express.static('public'))
-
-app.post("/upload", upload.single('newFile'),(req, res) => {
-    // use modules such as express-fileupload, Multer, Busboy
-    // const imgname = req.file.originalname
-    // if(!req.file){
-    //     res.status(422).json({result:422,msg:"please upload image"})
-    // }
-    // try{
-    //     conn.query("INSERT INTO Images SET ?",{userimg:imgname},(err,res)=>{
-    //         if(err){
-    //             console.log("ddddd"+imgname)
-    //         }else{
-    //             console.log("data added")
-    //         }
-    //     })
-    // }catch(error){
-    //     res.status(422).json({result:422,error})
-    // }
-    // setTimeout(() => {
-    //     console.log('file uploaded')
-    //     return res.status(200).json({ result: true, msg: 'file uploaded' });
-    // }, 3000);
+// Connect App routes
+app.use('/api-docs', openapi);
+app.use('/persons', person);
+app.use('*', (_, res) => {
+  res.redirect('/api-docs');
 });
 
-app.get("/getdata",async ()=>{
-    try{
-        await conn.query(`SELECT * FROM Images`,(err,res)=>{
-            if(err){
-                console.log("error")
-            }else{
-                console.log('this i'+res)
-                var columns = "";
-                for (var column in res.recordset.columns) {
-                    columns += column + ", ";
-                }
-                console.log("%s\t", columns.substring(0, columns.length - 2));
-        
-                // ouput row contents from default record set
-                res.recordset.forEach(row => {
-                    console.log("%s\t%s", row.CategoryName, row.ProductName);
-                });
-            }
-        })
-        console.log('nothing')
-    }catch(error){
-        res.status(422).json({result:422,error})
-    }
-})
-
-app.delete("/upload", (req, res) => {
-    
-    console.log(`File deleted`)
-    return res.status(200).json({ result: true, msg: 'file deleted' });
-});
-
-app.listen(8080, () => {
-    console.log(`Server running on port 8080`)
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
